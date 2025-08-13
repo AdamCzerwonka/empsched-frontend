2e128a0a7b733747e64a2a0f2c897f46b5eef97c
MarcelBadek <kriollik@gmail.com>
2025-08-13 11:25:31 +0200

    ref: change default shadcn colors

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,5 +1,8 @@
 import { useTranslation } from "react-i18next";
 import { Card, CardContent } from "../ui/card";
+import { Button } from "../ui/button";
+import { Link } from "react-router";
+import { navigation } from "~/constants";
 
 export const Footer = () => {
   const { t } = useTranslation("layout/footer");
@@ -7,9 +10,36 @@ export const Footer = () => {
   return (
     <Card className="min-h-16 w-full">
       <CardContent className="flex flex-row flex-wrap items-center justify-center gap-2 md:gap-6">
-        <h3>{t("aboutUs")}</h3>
-        <h3>{t("contactUs")}</h3>
-        <h3>{t("faqs")}</h3>
+        <h3>
+          <Button
+            variant={"link"}
+            className="text-foreground font-normal"
+            size="sm"
+            asChild
+          >
+            <Link to={navigation.home}>{t("aboutUs")}</Link>
+          </Button>
+        </h3>
+        <h3>
+          <Button
+            variant={"link"}
+            className="text-foreground font-normal"
+            size="sm"
+            asChild
+          >
+            <Link to={navigation.home}>{t("contactUs")}</Link>
+          </Button>
+        </h3>
+        <h3>
+          <Button
+            variant={"link"}
+            className="text-foreground font-normal"
+            size="sm"
+            asChild
+          >
+            <Link to={navigation.home}>{t("faqs")}</Link>
+          </Button>
+        </h3>
       </CardContent>
     </Card>
   );
