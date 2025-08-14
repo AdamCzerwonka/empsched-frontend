fa6a747c0abf6f02aeb63bd0603c6d6259c964e4
MarcelBadek <kriollik@gmail.com>
2025-08-14 12:07:31 +0200

    feat: add custom form field

---
+++ app/components/form/CustomFormField.tsx
@@ -0,0 +1,32 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+interface Props extends React.ComponentProps<"input"> {
+  name: string;
+  label: string;
+  placeholder: string;
+  type?: string;
+}
+
+export const CustomFormField = ({ name, label, ...props }: Props) => {
+  return (
+    <FormField
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          {label && <FormLabel>{label}</FormLabel>}
+          <FormControl>
+            <Input {...props} {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
