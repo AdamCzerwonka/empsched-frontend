d9af806691fa876a8a841be07c3a4ac83880dbc3
mz403079 <mz403079@gmail.com>
2026-01-23 00:08:26 +0100

    feat: add serwist service worker and manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { serwist } from "@serwist/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    serwist({
+      swSrc: "src/sw.ts",
+      swDest: "sw.js",
+      // globDirectory: "dist",
+      globDirectory: "build/client",
+      injectionPoint: "self.__SW_MANIFEST",
+      rollupFormat: "iife",
+    }),
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./app"),
